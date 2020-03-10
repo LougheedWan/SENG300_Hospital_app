e21e1d8e15f991aa5048c6e16c75ca51ceda7bba
LougheedWan <lougheedwan123@hotmail.com>
2020-03-10 13:36:01 -0600

    more stuff

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
@@ -112,6 +114,7 @@ public class Main extends Application {
 		// scenes
 		Group login = new Group();
 		Scene login_scene = new Scene(login, 1280, 720);
+		login_scene.setFill(Color.AQUA);
 		
 		Group main = new Group();
 		Scene main_scene = new Scene(main, 1280, 720);
@@ -144,10 +147,11 @@ public class Main extends Application {
 		error_lbl.setLayoutY(375);
 		
 		Label welcome_lbl = new Label("Welcome!");
-		welcome_lbl.setTextFill(Color.web("#0076a3"));
-		welcome_lbl.setLayoutX(555);
-		welcome_lbl.setLayoutY(275);
-		
+		welcome_lbl.setTextFill(Color.BLACK);
+		welcome_lbl.setLayoutX(540);
+		welcome_lbl.setLayoutY(290);
+		Font font = Font.font(16);
+		welcome_lbl.setFont(font);
 		
 		// Add to group
 		//System.out.println("adding children");
@@ -175,8 +179,8 @@ public class Main extends Application {
 		appo_title.setTextFill(Color.web("#0076a3"));
 		appo_title.setLayoutX(0);
 		appo_title.setLayoutY(0);
-		Font font = Font.font(25);
-		appo_title.setFont(font);
+		Font fonts = Font.font(25);
+		appo_title.setFont(fonts);
 		
 		Label name = new Label("name");
 		name.setLayoutX(0);
@@ -188,7 +192,29 @@ public class Main extends Application {
 		name_input.setLayoutX(0);
 		name_input.setLayoutY(50);
 		
-		new_appo.getChildren().addAll(appo_title, name, name_input);
+		Label email = new Label("Email");
+		email.setLayoutX(0);
+		email.setLayoutY(75);
+		Font email_font = Font.font(16);
+		email.setFont(email_font);
+		
+		TextField email_input = new TextField("input email");
+		email_input.setLayoutX(0);
+		email_input.setLayoutY(95);
+		
+		Label doctor = new Label("Select Doctor");
+		doctor.setLayoutX(0);
+		doctor.setLayoutY(120);
+		doctor.setFont(email_font);
+		
+		String doc[] = {"Bob Ross", "Chris Lee", "James Bond"};
+		
+		ChoiceBox doc_choose = new ChoiceBox(FXCollections.observableArrayList(doc));
+		doc_choose.setLayoutX(0);
+		doc_choose.setLayoutY(140);
+		
+		
+		new_appo.getChildren().addAll(appo_title, name, name_input, email, email_input, doctor, doc_choose);
 		
 		
 		login_butt.setOnAction(new EventHandler<ActionEvent>() {
@@ -198,6 +224,8 @@ public class Main extends Application {
 				if (checkLogin(user_txt.getText(), pass_txt.getText())) {
 					setUser(user_txt.getText(), pass_txt.getText());
 					name_user.setText("Welcome " + current_user);
+					Font main_font = Font.font(20);
+					name_user.setFont(main_font);
 					primaryStage.setScene(main_scene);
 				}
 				else {
