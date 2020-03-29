dc13475e03314ee18825924c8b625c4cea1ba5a7
alexislee1 <47426034+alexislee1@users.noreply.github.com>
2020-03-28 20:38:55 -0600

    Personal information GUI

--- Main.java
+++ Main.java
@@ -31,12 +31,14 @@ public class Main extends Application {
 	private String current_type;
 	public static User current;
 	private Stage secondStage;
+	private Stage newStage;
 	private static TextField name_input;
 	private static TextField email_input;
+	private static TextField name1_input;
 	private static TextArea reason_txt;
 	private static ChoiceBox doc_choose;
 	private static ListView<String> list;
-	
+
 	// Methods
 	public boolean checkLogin(String username, String password){
 		// check file IO for file titled given username
@@ -58,7 +60,7 @@ public class Main extends Application {
                         String temp2[] = temp.split(",");
                         un.add(temp2[0]);
                         pw.add(temp2[1]);
-                       
+
                     }
                     for (int count =0; count<un.size(); count++) {
                     	if(username.equals(un.get(count))) {
@@ -79,28 +81,28 @@ public class Main extends Application {
 		else
                     return false;
 	}
-	
-	
+
+
 	public void setUser(String username, String pw) {
 		// Pull and place in local variables all text file info
 		//currentUser = new User(username, password);
-		
-		
+
+
 		try {
 			BufferedReader scan = new BufferedReader(new FileReader("userdata/" + username + ".txt"));
 			current_user = scan.readLine();
 			System.out.println(current_user);
 			current_type = scan.readLine();
 			System.out.println(current_type);
-			
+
 			//make if statement to determine what object to make
-			
-			if (current_type.equals("Doctor")) {  
+
+			if (current_type.equals("Doctor")) {
 				current = new Doctor(current_user, current_type, username);
-				
+
 				//from here on, anything that is in the doctor class the current user can do.
 				//we repeat the same thing for all classes below
-			} 
+			}
 			else if (current_type.equals("Patient")) {
 				current = new Patient(current_user, current_type, username);
 				//patient class methods can be used if object is patient.
@@ -111,14 +113,14 @@ public class Main extends Application {
 			else if (current_type.equals("Admin")) {
 				current = new Admin(current_user, current_type, username);
 			}
-			
+
 			scan.close();
 		}
 		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
+
 	public static void store_appo_info(String file) {
 		try {
 			FileWriter writer = new FileWriter(file, true);
@@ -135,14 +137,14 @@ public class Main extends Application {
 			writer.write(temp);
 			writer.write("\n");
 			writer.close();
-			
-			
+
+
 		}
 		catch (Exception e) {
-			
+
 		}
 	}
-	
+
 	public static String get_doc_txt() {
 		String temp = (String)doc_choose.getValue();
 		System.out.println(temp);
@@ -155,7 +157,7 @@ public class Main extends Application {
 		else {
 			return " ";
 		}
-		
+
 	}
 	public static void goto_doctor(Group group) {
 		// Rectangles
@@ -168,33 +170,44 @@ public class Main extends Application {
 		app1_lbl.setLayoutX(100);
 		app1_lbl.setLayoutY(200);
 		app1_lbl.setFont(new Font("Arial", 24));
-		
-		group.getChildren().addAll(app1_rct, app2_rct, app1_lbl);
+
+		// Rectangle - patients list
+		Rectangle app1_rct_pa = new Rectangle(1000.00d, 200.00d, 400.00d, 600.00d);
+		app1_rct_pa.setFill(Color.GAINSBORO);
+		Rectangle app2_rct_pa = new Rectangle(1000.00d, 200.00d, 400.00d, 30.00d);
+		app2_rct_pa.setFill(Color.rgb(160, 0, 0, 0.7));
+		// Labels - patients list
+		Label app1_lbl_pa = new Label("My Patients");
+		app1_lbl_pa.setLayoutX(1170);
+		app1_lbl_pa.setLayoutY(200);
+		app1_lbl_pa.setFont(new Font("Arial", 24));
+
+		group.getChildren().addAll(app1_rct, app2_rct, app1_lbl, app1_rct_pa, app2_rct_pa, app1_lbl_pa);
 	}
-	
+
 	public static void goto_nurse(Group group) {
 		System.out.println("NURSE LOADED");
 		//group.getChildren().addAll();
 		}
-	
+
 	public static void goto_patient(Group group) {
 		System.out.println("PATIENT LOADED");
-		//group.getChildren().addAll();
+		//group.getChildren().addAll(info);
 		}
-	
+
 	public static void goto_admin(Group group) {
 		System.out.println("ADMIN LOADED");
 		//group.getChildren().addAll();
 		}
-	
-	
+
+
 	public static void base_gui(Group group) {
 		// Header Red Bar
 		Rectangle header_rct = new Rectangle(40.00d,100.00d, 1840.00d, 50.00d);
 		header_rct.setFill(Color.rgb(160, 0, 0));
 		// User Info
-		
-		
+
+
 		// HMS logo
 		Label logo_lbl = new Label("HMS");
 		logo_lbl.setFont(new Font("Arial", 110));
@@ -209,34 +222,37 @@ public class Main extends Application {
 		info2_lbl.setFont(new Font("Typewriter", 16));
 		info2_lbl.setLayoutX(290);
 		info2_lbl.setLayoutY(73);
-		
-		
+
+
 		group.getChildren().addAll(header_rct,logo_lbl, info1_lbl, info2_lbl);
 	}
-	
+
 	//Launch program
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		// scenes
 		Group login = new Group();
 		Scene login_scene = new Scene(login, 1920, 1080);
 		login_scene.setFill(Color.GHOSTWHITE);
-		
+
 		Group home = new Group();
 		Scene main_scene = new Scene(home, 1920, 1080);
-		
+
 		Group new_appo = new Group();
 		Scene new_appo_scene = new Scene(new_appo, 1000, 620);
-		
+
 		Group times = new Group();
 		Scene times_scene = new Scene(times, 1000, 500);
-		
+
+		Group pinfo = new Group();
+		Scene pinfo_scene = new Scene(pinfo, 1000, 620);
+
 		// 		LOGIN 		//
 		// buttons
 		Button login_butt = new Button("Login");
@@ -244,7 +260,7 @@ public class Main extends Application {
 		login_butt.setLayoutX(930);
 		login_butt.setLayoutY(600);
 		login_butt.setStyle("-fx-background-color: Cornsilk");
-		
+
 		// textfields
 		TextField user_txt = new TextField();
 		user_txt.setPromptText("username");
@@ -256,38 +272,38 @@ public class Main extends Application {
 		pass_txt.setPrefWidth(200);
 		pass_txt.setLayoutX(860);
 		pass_txt.setLayoutY(550);
-		
+
 		// label
 		Label error_lbl = new Label("username or password incorrect");
 		error_lbl.setTextFill(Color.web("#0076a3"));
 		error_lbl.setLayoutX(860);
 		error_lbl.setLayoutY(575);
-		
+
 		Label welcome_lbl = new Label("Welcome!");
 		welcome_lbl.setTextFill(Color.BLACK);
 		welcome_lbl.setLayoutX(860);
 		welcome_lbl.setLayoutY(480);
 		Font font = Font.font(16);
 		welcome_lbl.setFont(font);
-		
+
 		// Add to group
 		//System.out.println("adding children");
 		login.getChildren().addAll(user_txt, pass_txt, login_butt,welcome_lbl);
-		
-		
+
+
 		//		MAIN		//
-		
+
 		Label name_user = new Label();
 		name_user.setLayoutX(500);
 		name_user.setLayoutY(10);
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
 		// BUTTONS
 		Button new_apo = new Button("book appointment");
 		new_apo.setPrefWidth(100);
@@ -300,85 +316,108 @@ public class Main extends Application {
 		var_apo.setLayoutX(1700);
 		var_apo.setLayoutY(300);
 		var_apo.setStyle("-fx-background-color: Cornsilk");
-		
+
+		// personal information button
+		Button info = new Button("personal information");
+		info.setPrefWidth(100);
+		info.setLayoutX(600);
+		info.setLayoutY(100);
+		info.setWrapText(true);
+		info.setStyle("-fx-background-color: Cornsilk");
+
+// add personal information //
+	Label name_1 = new Label("Name");
+	name_1.setLayoutX(0);
+	name_1.setLayoutY(30);
+	Font name_1_font = Font.font(16);
+	name_1.setFont(name_1_font);
+
+	name1_input = new TextField("Name");
+	name1_input.setLayoutX(0);
+	name1_input.setLayoutY(50);
+
+
+	pinfo.getChildren().addAll(name_1, name1_input);
+
+
 //		new appointment		//
-		
+
 		Label appo_title = new Label("BOOK APPOINTMENT");
 		appo_title.setTextFill(Color.web("#0076a3"));
 		appo_title.setLayoutX(0);
 		appo_title.setLayoutY(0);
 		Font fonts = Font.font(25);
 		appo_title.setFont(fonts);
-		
+
 		Label name = new Label("name");
 		name.setLayoutX(0);
 		name.setLayoutY(30);
 		Font name_font = Font.font(16);
 		name.setFont(name_font);
-		
+
 		name_input = new TextField("input name");
 		name_input.setLayoutX(0);
 		name_input.setLayoutY(50);
-		
+
 		Label email = new Label("Email");
 		email.setLayoutX(0);
 		email.setLayoutY(75);
 		Font email_font = Font.font(16);
 		email.setFont(email_font);
-		
+
 		email_input = new TextField("input email");
 		email_input.setLayoutX(0);
 		email_input.setLayoutY(95);
-		
+
 		Label doctor = new Label("Select Doctor");
 		doctor.setLayoutX(0);
 		doctor.setLayoutY(120);
 		doctor.setFont(email_font);
-		
+
 		//hard code the names, it should be fine for now
 		String doc[] = {"Bob Ross", "Jacky Chan"};
-		
+
 		doc_choose = new ChoiceBox(FXCollections.observableArrayList(doc));
 		doc_choose.setLayoutX(0);
 		doc_choose.setLayoutY(140);
-		
+
 		Label reason = new Label("Reason:");
 		reason.setLayoutX(0);
 		reason.setLayoutY(160);
 		reason.setFont(email_font);
-		
+
 		reason_txt = new TextArea();
 		reason_txt.setLayoutX(0);
 		reason_txt.setLayoutY(180);
 		reason.setMaxSize(100, 50);
-		
+
 		Button view_times = new Button("view available times");
 		view_times.setLayoutX(400);
 		view_times.setLayoutY(500);
-		
+
 		new_appo.getChildren().addAll(appo_title, name, name_input, email, email_input, doctor, doc_choose, reason, reason_txt, view_times);
-		
+
 		//view times
-		
+
 		Label title = new Label("Your doctor has these times avaliable:");
 		title.setLayoutX(0);
 		title.setLayoutY(0);
 		title.setFont(fonts);
-		
+
 		list = new ListView<String>(Bookings.doc_times);
 		list.setLayoutX(0);
 		list.setLayoutY(40);
 		list.setPrefSize(400, 300);
-		
+
 		Button confirm_appo = new Button("Confirm");
 		confirm_appo.setLayoutX(500);
 		confirm_appo.setLayoutY(350);
-		
+
 		times.getChildren().addAll(title,list, confirm_appo);
-		
+
 		// BUTTON ACTIONS //
-		
-		
+
+
 		// Login
 		login_butt.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -391,8 +430,8 @@ public class Main extends Application {
 					name_user.setFont(main_font);
 					primaryStage.setScene(main_scene);
 					base_gui(home);
-					home.getChildren().addAll(new_apo,name_user,var_apo);
-					
+					home.getChildren().addAll(new_apo,name_user,var_apo, info);
+
 					if (current.getType().equals("Doctor")) {
 						goto_doctor(home);
 					}
@@ -405,15 +444,15 @@ public class Main extends Application {
 					else if (current.getType().equals("Admin")) {
 						goto_admin(home);
 					}
-					
-					
+
+
 				}
 				else {
 					login.getChildren().add(error_lbl);
 				}
 			}
 		});
-		
+
 		// New Appointmnet
 		new_apo.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -424,11 +463,11 @@ public class Main extends Application {
 				secondStage.setTitle("new appoitment");
 				secondStage.show();
 			}
-			
+
 		});
-		
+
 		view_times.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			public void handle(ActionEvent event) {
 				Bookings.get_doc_times(get_doc_txt());
 				secondStage.setScene(times_scene);
@@ -436,21 +475,31 @@ public class Main extends Application {
 				secondStage.show();
 			}
 		});
-		
+
 		confirm_appo.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			public void handle(ActionEvent event) {
 				store_appo_info("pending.txt");
 				secondStage.close();
-				
+
 				Alert alert = new Alert(AlertType.INFORMATION, "Thank you, your appointment is now pending.", ButtonType.OK);
 				alert.showAndWait();
 			}
 		});
-		
+
+		info.setOnAction(new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				newStage = new Stage();
+				newStage.setScene(pinfo_scene);
+				newStage.setTitle("Personal Information");
+				newStage.show();
+			}
+		});
+
 		// Canvas
 		primaryStage.setTitle("Hospital Management System");
-		
+
 		primaryStage.setScene(login_scene);
 		primaryStage.setResizable(true);
 		primaryStage.show();
